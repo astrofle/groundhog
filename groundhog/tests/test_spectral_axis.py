c7c5692a71dd4ffc9b51ec10c1ca5a0d5c75d0d0
astrofle <salas.mz@gmail.com>
2021-06-15 13:52:13 -0400

    Updated tests

--- groundhog/tests/test_spectral_axis.py
+++ groundhog/tests/test_spectral_axis.py
@@ -8,7 +8,7 @@ from groundhog import spectral_axis
 
 def test_compute_freq_axis(sd_fits_table, gbtidl_spec):
     #table,head = sd_fits_table
-    table = sd_fits_table.table
+    table = sd_fits_table.table[0]
     freq = spectral_axis.compute_freq_axis(table)
     np.testing.assert_allclose(freq.to('MHz').value[0], gbtidl_spec[:,0])
     
