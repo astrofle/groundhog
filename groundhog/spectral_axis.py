2c9941dd582f09110a28f6b4f374a63c19b964f3
astrofle <psalas@nrao.edu>
2022-08-22 13:35:31 -0400

    Spectral axis is not only a frequency axis

--- groundhog/spectral_axis.py
+++ groundhog/spectral_axis.py
@@ -7,14 +7,13 @@ from astropy import units as u
 from astropy import constants as ac
 
 
-def compute_freq_axis(table, chstart=1, chstop=-1, apply_doppler=True):
+def compute_spectral_axis(table, chstart=1, chstop=-1, apply_doppler=True):
     """
     """
 
     # Copied from GBT gridder: 
     # https://github.com/nrao/gbtgridder/blob/master/src/get_data.py
     
-    #shape = table.field('data').shape
     shape = table['DATA'].shape
     if len(shape) == 1:
         ax = 0
@@ -24,10 +23,16 @@ def compute_freq_axis(table, chstart=1, chstop=-1, apply_doppler=True):
     if chstop == -1:
         chstop = shape[ax] + 1
         
-    freq = np.zeros(shape)
+    xaxis = np.zeros(shape)
 
-    crv1 = table['CRVAL1']*u.Hz
-    cd1 = table['CDELT1']*u.Hz
+    try:
+        cunit1 = u.Unit(table["CUNIT1"][0])
+    except ValueError:
+        cunit1 = u.Hz
+
+
+    crv1 = table['CRVAL1']*cunit1
+    cd1 = table['CDELT1']*cunit1
     crp1 = table['CRPIX1']
     vframe = table['VFRAME']*u.m/u.s
 
@@ -39,13 +44,13 @@ def compute_freq_axis(table, chstart=1, chstop=-1, apply_doppler=True):
     if apply_doppler:
         doppler = np.sqrt((1.0 + beta)/(1.0 - beta))
     
-    # Full frequency axis in doppler tracked frame from first row.
-    # FITS counts from 1, this indx refers to the original axis, before chan selection.
+    # Full spectral axis in doppler tracked frame from first row.
+    # FITS counts from 1, this index refers to the original axis, before channel selection.
     indx = np.arange(chstop - chstart) + chstart
     if ax == 1:
         indx = np.tile(indx, (shape[0],1))
-        freq[:,:] = (crv1[:,np.newaxis] + cd1[:,np.newaxis]*(indx - crp1[:,np.newaxis]))*doppler[:,np.newaxis]
+        xaxis[:,:] = (crv1[:,np.newaxis] + cd1[:,np.newaxis]*(indx - crp1[:,np.newaxis]))*doppler[:,np.newaxis]
     else:
-        freq = (crv1 + cd1*(indx - crp1))*doppler
+        xaxis = (crv1 + cd1*(indx - crp1))*doppler
         
-    return freq*u.Hz
+    return xaxis*cunit1
