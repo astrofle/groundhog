476588e0094d9769cb1e9cd1ecc98ac124f70b18
astrofle <salas.mz@gmail.com>
2022-10-19 05:33:55 -0400

    Updated test for sd_fits_io

--- groundhog/tests/test_sd_fits_io.py
+++ groundhog/tests/test_sd_fits_io.py
@@ -13,5 +13,5 @@ def test_write_sdfits(sd_fits_table, tmp_path):
     d = tmp_path / "sd_fits_io"
     d.mkdir()
     out = d / "test.fits"
-    sd_fits_io.write_sdfits(out, sd_fits_table, overwrite=True)
+    sd_fits_io.write_sdfits(out, sd_fits_table.hdu[1][:], overwrite=True)
     assert len(list(tmp_path.iterdir())) == 1
